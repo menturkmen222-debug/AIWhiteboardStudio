8563373c7e2fc688288ac1c3041b669348745180
menturkmen222-debug <menturkmen222@gmail.com>
2026-01-07 07:27:49 +0500

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+// Top-level build file
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
+}
+
+buildscript {
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
+    }
+}
